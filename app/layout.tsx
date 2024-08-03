c98d5bc4bc92526d14a3874095874dfe08772706
VishnuVardhanBR <mmsvishnu23@gmail.com>
2024-08-04 02:38:53 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localfont from "next/font/local";
+import localfont from "@next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
